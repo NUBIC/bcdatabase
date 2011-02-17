a9fc3b62f661da980845c7529666c6dc1d69a986
Rhett Sutphin <rhett@detailedbalance.net>
2011-02-17 12:42:00 -0600

    This spec should accept .pre versions.

--- spec/bcdatabase/version_spec.rb
+++ spec/bcdatabase/version_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path("../spec_helper", File.dirname(__FILE__))
 require "bcdatabase/version"
 
 describe Bcdatabase do
-  it "should have a d.d.d version" do
-    Bcdatabase::VERSION.should =~ /^\d+\.\d+\.\d+$/
+  it "should have a d.d.d or d.d.d.text version" do
+    Bcdatabase::VERSION.should =~ /^\d+\.\d+\.\d+(\.\S+)?$/
   end
 end
